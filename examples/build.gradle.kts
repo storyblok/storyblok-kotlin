eb99b2ae9e59f4576082b9720136f3b3eafa6ea4
Nick Williams <nbransby@gmail.com>
2026-01-28 08:33:05 +0000

    test(examples): run examples on all supported platforms

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,10 +1,14 @@
-//import com.android.build.api.dsl.androidLibrary
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeSimulatorTest
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-//    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
+
 kotlin {
     jvm()
     js {
@@ -19,44 +23,63 @@ kotlin {
             }
         }
     }
-
-//    androidLibrary {
-//        namespace = "com.storyblok.ktor-client-storyblok"
-//        compileSdk = libs.versions.android.compileSdk.get().toInt()
-//        minSdk = libs.versions.android.minSdk.get().toInt()
-//
-//        withJava() // enable java compilation support
-//        withHostTestBuilder {}.configure {}
-//        withDeviceTestBuilder {
-//            sourceSetTreeName = "test"
-//        }
-//
-//        compilations.configureEach {
-//            compilerOptions.configure {
-//                jvmTarget.set(
-//                    JvmTarget.JVM_11
-//                )
-//            }
-//        }
-//    }
-//    iosX64()
-//    iosArm64()
-//    iosSimulatorArm64()
-//    linuxX64()
+    androidLibrary {
+        namespace = "com.storyblok"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        withHostTest {}
+        compilations.configureEach {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_11)
+                }
+            }
+        }
+    }
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    linuxArm64()
+    linuxX64()
+    macosArm64()
+    macosX64()
+    mingwX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    wasmJs {
+        nodejs()
+    }
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+    watchosX64()
 
     sourceSets {
 
         commonTest.dependencies {
             implementation(project(":ktor-client-plugin"))
             implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.coroutines.test)
-            implementation(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.client.logging)
         }
 
         jvmTest.dependencies {
             implementation(libs.logback.classic)
         }
+    }
+}
+
+tasks.withType<KotlinNativeSimulatorTest>().configureEach {
+    standalone.set(false)
+    device.set("booted")
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
     }
 }
