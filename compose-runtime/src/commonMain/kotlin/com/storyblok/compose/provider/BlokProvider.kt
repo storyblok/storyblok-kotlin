fd3624cc1b37208f980b13dbbbc4566d2b94cd31
Nick Williams <nbransby@gmail.com>
2026-03-04 14:57:50 +0000

    feat: draft compose-runtime

---
+++ compose-runtime/src/commonMain/kotlin/com/storyblok/compose/provider/BlokProvider.kt
@@ -0,0 +1,31 @@
+package com.storyblok.compose.provider
+
+import androidx.compose.runtime.Composable
+import com.storyblok.cdn.schema.Component
+import com.storyblok.compose.BlokScope
+import com.storyblok.compose.BlokScopeImpl
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlin.reflect.KClass
+
+public class BlokProvider internal constructor(
+    internal val blokScope: BlokScope,
+    internal val serializersModule: SerializersModule
+)
+
+public fun blokProviderWithoutRichText(
+    fallback: @Composable (unknownComponent: Component) -> Unit = { throw IllegalStateException("Unknown component ${it.component}") },
+    builder: BlokProviderScope.() -> Unit,
+): BlokProvider {
+    val providers = mutableMapOf<KClass<*>, Provider>(Component.Unknown::class to Provider.Blok(fallback))
+    val blokScope = BlokScopeImpl(providers)
+    return BlokProvider(
+        blokScope,
+        SerializersModule {
+            polymorphic(Component::class) {
+                BlokProviderScope(blokScope, providers, this)
+                    .apply { builder() }
+            }
+        }
+    )
+}
