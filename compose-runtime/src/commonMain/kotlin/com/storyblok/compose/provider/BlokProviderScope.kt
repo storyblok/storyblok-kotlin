6bd07a991741030ed95df9df1f40087c6fe2fe3a
Nick Williams <nbransby@gmail.com>
2026-03-13 12:49:48 +0000

    feat(compose): accept a generic context arg in Blok() builders, add headless blok registration

--- compose-runtime/src/commonMain/kotlin/com/storyblok/compose/provider/BlokProviderScope.kt
+++ compose-runtime/src/commonMain/kotlin/com/storyblok/compose/provider/BlokProviderScope.kt
@@ -16,45 +16,55 @@ public class BlokProviderScope internal constructor(
     private val polymorphicModuleBuilder: PolymorphicModuleBuilder<Component>
 ) : BlokScope by blokScope {
 
-    public fun <T : Component> blok(type: KClass<T>, serializer: KSerializer<T>, composable: @Composable (T) -> Unit) {
-        require(type !in providers) { "A blok for ${type.simpleName} has already been registered" }
-        providers[type] = Provider.Blok(composable)
+    /**
+     * Registers headless a blok.
+     */
+    public fun <T : Component> blok(type: KClass<T>, serializer: KSerializer<T>) {
         polymorphicModuleBuilder.subclass(type, serializer)
     }
 
     public fun <T : Component> blok(type: KClass<T>, serializer: KSerializer<T>, composable: @Composable (T, Modifier) -> Unit) {
         require(type !in providers) { "A blok for ${type.simpleName} has already been registered" }
-        providers[type] = Provider.ModifiableBlok(composable)
+        providers[type] = Provider.Blok(composable)
         polymorphicModuleBuilder.subclass(type, serializer)
     }
 
-    public fun <T : RichText> richText(type: KClass<T>, composable: @Composable (T) -> Unit) {
+    public fun <T : Component, S> blok(type: KClass<T>, serializer: KSerializer<T>, composable: @Composable (T, S, Modifier) -> Unit) {
+        require(type !in providers) { "A blok for ${type.simpleName} has already been registered" }
+        providers[type] = Provider.BlokWithContext(composable)
+        polymorphicModuleBuilder.subclass(type, serializer)
+    }
+
+    public fun <T : RichText> richText(type: KClass<T>, composable: @Composable (T, Modifier) -> Unit) {
         require(type !in providers) { "A rich text blok for ${type.simpleName} has already been registered" }
         providers[type] = Provider.RichText(composable)
     }
 
-    public fun <T : RichText> richText(type: KClass<out T>, composable: @Composable (T, Modifier) -> Unit) {
-        require(type !in providers) { "A rich text blok for ${type.simpleName} has already been registered" }
-        providers[type] = Provider.ModifiableRichText(composable)
-    }
-
-    public fun <T : RichText> defaultRichText(type: KClass<out T>, composable: @Composable (T) -> Unit) {
+    public fun <T : RichText> defaultRichText(type: KClass<out T>, composable: @Composable (T, Modifier) -> Unit) {
         providers.getOrPut(type) { Provider.RichText(composable) }
     }
 
-    public inline fun <reified T : Component> blok(noinline composable: @Composable (T) -> Unit): Unit =
-        blok(T::class, serializer<T>(), composable)
+    public fun <T : RichText, S> richText(type: KClass<out T>, composable: @Composable (T, S, Modifier) -> Unit) {
+        require(type !in providers) { "A rich text blok for ${type.simpleName} has already been registered" }
+        providers[type] = Provider.RichTextWithContext(composable)
+    }
+
+    public inline fun <reified T : Component> blok(): Unit =
+        blok(T::class, serializer<T>())
 
     public inline fun <reified T : Component> blok(noinline composable: @Composable (T, Modifier) -> Unit): Unit =
         blok(T::class, serializer<T>(), composable)
 
-    public inline fun <reified T : RichText> richText(noinline composable: @Composable (T) -> Unit): Unit =
-        richText(T::class, composable)
+    public inline fun <reified T : Component, S> blok(noinline composable: @Composable (T, S, Modifier) -> Unit): Unit =
+        blok(T::class, serializer<T>(), composable)
 
     public inline fun <reified T : RichText> richText(noinline composable: @Composable (T, Modifier) -> Unit): Unit =
         richText(T::class, composable)
 
-    public inline fun <reified T : RichText> defaultRichText(noinline composable: @Composable (T) -> Unit) {
+    public inline fun <reified T : RichText, S> richText(noinline composable: @Composable (T, S, Modifier) -> Unit): Unit =
+        richText(T::class, composable)
+
+    public inline fun <reified T : RichText> defaultRichText(noinline composable: @Composable (T, Modifier) -> Unit) {
         defaultRichText(T::class, composable)
     }
 }
