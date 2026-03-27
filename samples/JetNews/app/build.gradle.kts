31f1815a95a8d3da3ee08cbe66fd3c501d3dfb17
Nick Williams <nbransby@gmail.com>
2026-03-27 12:28:00 +0000

    build(samples/JetNews): add Storyblok dependencies

--- samples/JetNews/app/build.gradle.kts
+++ samples/JetNews/app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.storyblok.compose.android)
+    implementation(libs.storyblok.material3.android)
 }
