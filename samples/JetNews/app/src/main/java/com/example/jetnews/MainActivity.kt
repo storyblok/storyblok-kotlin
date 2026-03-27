7b218e5440bae9485bef57e90bbb07b40bc75d38
Nick Williams <nbransby@gmail.com>
2026-03-27 12:24:47 +0000

    feat(samples/JetNews): New Empty Activity Project

---
+++ samples/JetNews/app/src/main/java/com/example/jetnews/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.jetnews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnews.ui.theme.JetNewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetNewsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                }
+            }
+        }
+    }
+}
