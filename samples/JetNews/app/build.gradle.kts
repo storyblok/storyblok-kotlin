6627c6c628c734a951c44f6d350112a1eebb511d
Nick Williams <nbransby@gmail.com>
2026-04-02 14:35:17 +0100

    build(samples/JetNews): add dynamic routing using navigation 3

--- samples/JetNews/app/build.gradle.kts
+++ samples/JetNews/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation3.runtime)
     implementation(libs.coil.compose)
     implementation(libs.coil.network.ktor3)
     implementation(libs.kotlinx.datetime)
