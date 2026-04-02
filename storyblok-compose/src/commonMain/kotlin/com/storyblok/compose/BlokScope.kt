2a83116f38ccdd6a63524ba743439e9aac07ee09
Nick Williams <nbransby@gmail.com>
2026-04-02 12:23:24 +0100

    refactor(storyblok-compose): remove Blok with generic context - users should use CompositionLocalProvider instead

--- storyblok-compose/src/commonMain/kotlin/com/storyblok/compose/BlokScope.kt
+++ storyblok-compose/src/commonMain/kotlin/com/storyblok/compose/BlokScope.kt
@@ -13,24 +13,17 @@ public interface BlokScope {
     @Composable
     public fun Blok(content: Component, modifier: Modifier = Modifier)
     @Composable
-    public fun <T> Blok(content: Component, context: T, modifier: Modifier = Modifier)
-    @Composable
     public fun RichText(content: RichText, modifier: Modifier = Modifier)
     public fun AnnotatedString.Builder.RichText(content: RichText)
 }
 
 @JvmInline
 internal value class BlokScopeImpl internal constructor(val providers: Map<Any, Provider>) : BlokScope {
+
     @Composable
     override fun Blok(content: Component, modifier: Modifier) =
         providers.getOrElse(content::class) { error("No blok registered for ${content.component}") }
             .invoke(content, modifier)
-
-    @Composable
-    override fun <T> Blok(content: Component, context: T, modifier: Modifier) {
-        providers.getOrElse(content::class) { error("No blok registered for ${content.component}") }
-            .invoke(content, context, modifier)
-    }
 
     @Composable
     override fun RichText(content: RichText, modifier: Modifier) =
