5d255803ac435b13457bffcba3ecbbb5519c2733
Nick Williams <nbransby@gmail.com>
2026-04-20 12:19:08 +0100

    docs(content-ap-client): tweak README.md

--- content-api-client/src/commonMain/kotlin/com/storyblok/serializers/StoryblokDateTimeSerializer.kt
+++ content-api-client/src/commonMain/kotlin/com/storyblok/serializers/StoryblokDateTimeSerializer.kt
@@ -6,6 +6,19 @@ import kotlinx.datetime.LocalTime
 import kotlinx.datetime.format.char
 import kotlinx.datetime.serializers.FormattedLocalDateTimeSerializer
 
+/**
+ * Serializer for [LocalDateTime] values using the Storyblok date-time format (`yyyy-MM-dd HH:mm:ss`).
+ *
+ * #### Example
+ * ```kotlin
+ * @Serializable
+ * @SerialName("event")
+ * class Event(
+ *     @Serializable(with = StoryblokDateTimeSerializer::class)
+ *     val date: LocalDateTime
+ * ) : Component()
+ * ```
+ */
 public object StoryblokDateTimeSerializer : FormattedLocalDateTimeSerializer(
     "com.storyblok.serializers.StoryblokDateTime",
     LocalDateTime.Format {
