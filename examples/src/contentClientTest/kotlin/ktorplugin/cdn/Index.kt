93cd57a27da716a76ea3c59323eabb6535f8f57e
Nick Williams <nbransby@gmail.com>
2026-09-04 09:28:06 +0100

    feat(example): update story examples to use StoryblokClient

---
+++ examples/src/contentClientTest/kotlin/ktorplugin/cdn/Index.kt
@@ -0,0 +1,33 @@
+package ktorplugin.cdn
+
+import com.storyblok.ktor.Api.Config.Version.*
+import com.storyblok.cdn.StoryblokClient
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.first
+import androidx.paging.asItemSnapshotListFlow
+import kotlin.test.Test
+
+class Index {
+
+    /**
+     * Learn the basics of the Storyblok Content Delivery API, including authentication, caching, CDN, pagination, rate limits, and error handling.
+     * https://www.storyblok.com/docs/api/content-delivery/v2/index
+     */
+    @Test
+    fun Introduction() = runBlocking {
+
+        // implementation("com.storyblok:content-api-client:0.5.0")
+        val client = StoryblokClient(
+            accessToken = "wANpEQEsMYGOwLxwXQ76Ggtt",
+            version = Published,
+        )
+        
+        val stories = client
+            .stories()
+            .asItemSnapshotListFlow()
+            .first()
+        
+        println(stories)
+    }
+
+}
